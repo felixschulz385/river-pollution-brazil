880a2123ca1c75f58181a0e5ab1843615dbb5e60
felixschulz385 <felix@zoelch-schulz.de>
2025-11-27 13:03:06 +0100

    Preliminary updated version of reachability parsing - feat: Update reachability parsing to use NetworkX functions - exp: Add notebook with experiments

--- code/data/river_network.py
+++ code/data/river_network.py
@@ -7,6 +7,7 @@ import shapely
 from sklearn.cluster import MiniBatchKMeans
 import multiprocessing as mp
 from multiprocessing import Pool
+from joblib import Parallel, delayed
 import networkx as nx
 from tqdm import tqdm
 from collections import defaultdict
@@ -470,52 +471,59 @@ def create_graph(edge_list):
     G.add_edges_from(edge_list)
     return G
 
-@lru_cache(maxsize=None)
-def find_reachable_nodes(G, start_node):
-    """ Find all reachable nodes from a given node using DFS. """
-    # We use a set to avoid duplicates
-    visited = set()
-    # Stack for DFS
-    stack = [start_node]
-    while stack:
-        node = stack.pop()
-        if node not in visited:
-            visited.add(node)
-            # Add all neighbors to the stack
-            stack.extend(G.successors(node))
-    return frozenset(visited)  # Return as an immutable set for caching
+
+def find_reachable_nodes_nx(G, node):
+    """ Find all reachable nodes using NetworkX's optimized descendants. """
+    reachable = nx.descendants(G, node)
+    reachable.add(node)  # Include the node itself
+    return node, reachable
+
 
 def compute_reachability_parallel(graph):
     """ Compute reachability for each node in the graph in parallel. """
+    n_nodes = len(graph)
     
-    # If the graph is small, we don't need multiprocessing
-    if len(graph) < 1000:
-        n_cores = 1
-    else:
-        n_cores = int(os.environ["SLURM_CPUS_PER_TASK"])
+    # For small graphs, no parallelization needed
+    if n_nodes < 500:
+        return {node: nx.descendants(graph, node) | {node} for node in graph.nodes()}
     
-    with mp.Pool(n_cores) as pool:
-        # Get all nodes in the graph
-        nodes = list(graph.nodes())
-        # Prepare the partial function with fixed graph
-        func = partial(find_reachable_nodes, graph)
-        # Map the function across all nodes using multiprocessing
-        results = pool.map(func, nodes)
-        # Turn frozensets back into sets
-        results = [set(x) for x in results]
-        # Return the results as a dictionary
-        return zip(nodes, results)
+    # For larger graphs, use parallel processing
+    n_cores = int(os.environ.get("SLURM_CPUS_PER_TASK", os.cpu_count() or 4))
+    
+    results = Parallel(n_jobs=n_cores, prefer="threads")(
+        delayed(find_reachable_nodes_nx)(graph, node) for node in graph.nodes()
+    )
+    
+    return dict(results)
+
 
 def compute_reachability():
     # Import the river network shapefile
-    rivers_brazil_shapefile = gpd.read_feather("/pfs/work7/workspace/scratch/tu_zxobe27-master_thesis/data/river_network/shapefile.feather")
+    rivers_brazil_shapefile = gpd.read_feather(
+        "/Users/felixschulz/Library/CloudStorage/OneDrive-Personal/Dokumente/Uni/Masterthesis/data/river_network/shapefile.feather"
+    )
     
-    # Iterate over all estuaries
-    for i in tqdm(rivers_brazil_shapefile.estuary.dropna().unique()):
-        edges = rivers_brazil_shapefile.query(f"estuary=={i}").copy().loc[:, ["downstream_node_id", "upstream_node_id"]].dropna().values
-        # Create the graph
-        G = create_graph(edges)
-        # Compute the reachability
-        with shelve.open("/pfs/work7/workspace/scratch/tu_zxobe27-master_thesis/data/river_network/reachability.db") as reachability:
-            for x in compute_reachability_parallel(G):
-                reachability[str(int(x[0]))] = x[1] 
+    db_path = "/Users/felixschulz/Library/CloudStorage/OneDrive-Personal/Dokumente/Uni/Masterthesis/data/river_network/reachability.db"
+    
+    estuaries = rivers_brazil_shapefile.estuary.dropna().unique()
+    
+    # Open shelve once for entire computation (much more efficient)
+    with shelve.open(db_path, flag='c', protocol=-1) as reachability_db:
+        for i in tqdm(estuaries):
+            edges = (
+                rivers_brazil_shapefile
+                .query(f"estuary=={i}")
+                .loc[:, ["downstream_node_id", "upstream_node_id"]]
+                .dropna()
+                .values
+            )
+            
+            G = create_graph(edges)
+            reachability_dict = compute_reachability_parallel(G)
+            
+            # Batch write results
+            for node, reachable_set in reachability_dict.items():
+                reachability_db[str(int(node))] = reachable_set
+        
+        # Sync to disk at the end
+        reachability_db.sync()
